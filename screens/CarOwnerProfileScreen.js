f082c1ec793d0a036ba92d77afd2acac92102155
AlessandroMazza28 <168297329+AlessandroMazza28@users.noreply.github.com>
2024-05-29 01:55:24 +0300

    Create CarOwnerProfileScreen.js

---
+++ screens/CarOwnerProfileScreen.js
@@ -0,0 +1,108 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import COLORS from "../constants/colors";
+
+const CarOwnerProfileScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={require("../assets/facebook.png")}
+          style={styles.profileImage}
+        />
+        <Text style={styles.userName}>User Name</Text>
+        <Text style={styles.userType}>Car Owner</Text>
+      </View>
+      <View style={styles.menuContainer}>
+        <TouchableOpacity
+          style={styles.menuItem}
+          onPress={() => navigation.navigate("MapViewNearby")}
+        >
+          <Text style={styles.menuItemText}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.menuItem}
+          onPress={() => navigation.navigate("SubscriptionPlan")}
+        >
+          <Text style={styles.menuItemText}>My Subscription</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.menuItem}
+          onPress={() => navigation.navigate("VechicleDocuments")}
+        >
+          <Text style={styles.menuItemText}>My Vehicle</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.menuItem}
+          onPress={() =>
+            navigation.navigate("SettingsScreen", { userType: "carOwner" })
+          }
+        >
+          <Text style={styles.menuItemText}>Settings</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.menuItem}
+          onPress={() => navigation.navigate("SwitchServiceTypeScreen")}
+        >
+          <Text style={styles.menuItemText}>Switch</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.menuItem}
+          onPress={() => navigation.navigate("Logout")}
+        >
+          <Text style={styles.menuItemText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 250,
+    marginBottom: 50,
+    backgroundColor: COLORS.darkBlue,
+  },
+  header: {
+    marginTop: 70,
+    alignItems: "center",
+    paddingVertical: 20,
+    backgroundColor: COLORS.darkBlue,
+    borderBottomWidth: 1,
+    borderBottomColor: "#eee",
+  },
+  profileImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+  },
+  userName: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 5,
+    color: COLORS.white,
+  },
+  userType: {
+    fontSize: 14,
+    color: "#777",
+    color: COLORS.white,
+  },
+  menuContainer: {
+    padding: 20,
+    height: 900,
+    backgroundColor: COLORS.white,
+  },
+  menuItem: {
+    paddingVertical: 25,
+    borderBottomWidth: 1,
+    borderBottomColor: COLORS.grey,
+  },
+  menuItemText: {
+    fontSize: 16,
+    color: "#333",
+    fontWeight: "bold",
+  },
+});
+
+export default CarOwnerProfileScreen;
