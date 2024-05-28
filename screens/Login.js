b9840e597e4b5b3acf9d9a4c090bbf7ce5d21348
AlessandroMazza28 <168297329+AlessandroMazza28@users.noreply.github.com>
2024-05-29 01:56:10 +0300

    Update Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -1,20 +1,69 @@
-/*
+//chatGPT
+import React, { useState } from "react";
 import {
   View,
   Text,
-  Image,
-  Pressable,
   TextInput,
   TouchableOpacity,
+  Alert,
+  Pressable,
+  Image,
 } from "react-native";
-import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import COLORS from "../constants/colors";
 import { Ionicons } from "@expo/vector-icons";
 import Button from "../components/Button";
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
 
-const Login = ({ navigation }) => {
+export default function Login({ navigation, login }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isPasswordShown, setIsPasswordShown] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleLogin = async () => {
+    const data = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await axios.post(
+        "https://newcaraid.onrender.com/api/users/login",
+        data
+      );
+
+      // Log the entire response for debugging
+      console.log("Response:", response);
+
+      // Access token and status code
+      const token = response.data.accessToken;
+      const statusCode = response.status;
+
+      // Log token and status code for debugging
+      console.log("Token:", token);
+      console.log("Status Code:", statusCode);
+
+      // Check if there is an error message
+      if (response.data.errorMessage) {
+        setErrorMessage(response.data.errorMessage);
+        return; // Exit if there is an error message
+      }
+
+      // Handle successful login
+      if (token) {
+        await SecureStore.setItemAsync("userToken", token);
+        navigation.navigate("MapViewNearby");
+        login(); // Call login function
+      }
+    } catch (error) {
+      // Log the error for debugging
+      console.log("Error:", error);
+
+      // Set error message
+      setErrorMessage(error.message);
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
@@ -63,6 +112,8 @@ const Login = ({ navigation }) => {
               style={{
                 width: "100%",
               }}
+              value={email}
+              onChangeText={setEmail}
             />
           </View>
         </View>
@@ -83,10 +134,12 @@ const Login = ({ navigation }) => {
             <TextInput
               placeholder="Password"
               placeholderTextColor={COLORS.black}
-              secureTextEntry={isPasswordShown}
+              secureTextEntry={!isPasswordShown}
               style={{
                 width: "100%",
               }}
+              value={password}
+              onChangeText={setPassword}
             />
 
             <TouchableOpacity
@@ -96,7 +149,7 @@ const Login = ({ navigation }) => {
                 right: 12,
               }}
             >
-              {isPasswordShown == true ? (
+              {isPasswordShown ? (
                 <Ionicons name="eye-off" size={24} color={COLORS.black} />
               ) : (
                 <Ionicons name="eye" size={24} color={COLORS.black} />
@@ -108,9 +161,7 @@ const Login = ({ navigation }) => {
         <Button
           title="Login"
           filled
-          onPress={() => {
-            navigation.push("MapViewNearby");
-          }}
+          onPress={handleLogin}
           style={{
             marginTop: 18,
             marginBottom: 4,
@@ -224,273 +275,4 @@ const Login = ({ navigation }) => {
       </View>
     </SafeAreaView>
   );
-};
-
-export default Login;
-*/
-
-
-//chatGPT
-import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  Alert,
-  Pressable,
-  Image,
-} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import COLORS from '../constants/colors';
-import { Ionicons } from '@expo/vector-icons';
-import Button from '../components/Button';
-import axios from "axios";
-import * as SecureStore from "expo-secure-store";
-
-
-export default function Login ({navigation, login}){
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isPasswordShown, setIsPasswordShown] = useState(false);
-  const[errorMessage, setErrorMessage]=useState('');
-  const handleLogin =  () => {
-    const data={
-      email: email,
-      password:password
-    };
-      axios 
-      .post ("https://newcaraid.onrender.com/api/users/login", data)
-      .then(async(response)=>{
-        console.log(response);
-        if (response.data){
-          setErrorMessage(response.data.errorMessage)
-        }
-        if (response.data.token){
-          await SecureStore.setItemAsync("userToken", response.data.token);
-          login()
-        }
-      })
-      .catch((error)=>{
-        console.log(error)
-        if(error){
-          setErrorMessage(error)
-        }
-      });
-  
-  };
-  
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
-      <View style={{ flex: 1, marginHorizontal: 22 }}>
-        <View style={{ marginVertical: 22 }}>
-          <Text
-            style={{
-              fontSize: 32,
-              fontWeight: 'bold',
-              marginVertical: 12,
-              color: COLORS.black,
-              textAlign: 'center',
-            }}
-          >
-            Login
-          </Text>
-
-          <Text
-            style={{
-              fontSize: 16,
-              color: COLORS.black,
-              textAlign: 'center',
-            }}
-          >
-            Add your details to login
-          </Text>
-        </View>
-
-        <View style={{ marginBottom: 12 }}>
-          <View
-            style={{
-              width: '100%',
-              height: 48,
-              borderColor: COLORS.black,
-              borderWidth: 1,
-              borderRadius: 50,
-              alignItems: 'center',
-              justifyContent: 'center',
-              paddingLeft: 22,
-            }}
-          >
-            <TextInput
-              placeholder='Your Email'
-              placeholderTextColor={COLORS.black}
-              keyboardType='email-address'
-              style={{
-                width: '100%',
-              }}
-              value={email}
-              onChangeText={setEmail}
-            />
-          </View>
-        </View>
-
-        <View style={{ marginBottom: 12 }}>
-          <View
-            style={{
-              width: '100%',
-              height: 48,
-              borderColor: COLORS.black,
-              borderWidth: 1,
-              borderRadius: 50,
-              alignItems: 'center',
-              justifyContent: 'center',
-              paddingLeft: 22,
-            }}
-          >
-            <TextInput
-              placeholder='Password'
-              placeholderTextColor={COLORS.black}
-              secureTextEntry={!isPasswordShown}
-              style={{
-                width: '100%',
-              }}
-              value={password}
-              onChangeText={setPassword}
-            />
-
-            <TouchableOpacity
-              onPress={() => setIsPasswordShown(!isPasswordShown)}
-              style={{
-                position: 'absolute',
-                right: 12,
-              }}
-            >
-              {isPasswordShown ? (
-                <Ionicons name='eye-off' size={24} color={COLORS.black} />
-              ) : (
-                <Ionicons name='eye' size={24} color={COLORS.black} />
-              )}
-            </TouchableOpacity>
-          </View>
-        </View>
-
-        <Button
-          title='Login'
-          filled
-          onPress={handleLogin}
-          style={{
-            marginTop: 18,
-            marginBottom: 4,
-            borderRadius: 50,
-          }}
-        />
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'center',
-            marginVertical: 22,
-          }}
-        >
-          <Pressable onPress={() => navigation.navigate('Reset')}>
-            <Text
-              style={{
-                fontSize: 16,
-                color: COLORS.primary,
-                fontWeight: 'bold',
-                marginLeft: 6,
-              }}
-            >
-              Forget your password?
-            </Text>
-          </Pressable>
-        </View>
-
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginVertical: 20,
-          }}
-        >
-          <View
-            style={{
-              flex: 1,
-              height: 1,
-              backgroundColor: COLORS.grey,
-              marginHorizontal: 10,
-            }}
-          />
-          <Text style={{ fontSize: 14 }}>Or Login with</Text>
-          <View
-            style={{
-              flex: 1,
-              height: 1,
-              backgroundColor: COLORS.grey,
-              marginHorizontal: 10,
-            }}
-          />
-        </View>
-
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'center',
-          }}
-        >
-          <TouchableOpacity
-            onPress={() => console.log('Pressed')}
-            style={{
-              flex: 1,
-              alignItems: 'center',
-              justifyContent: 'center',
-              flexDirection: 'row',
-              height: 52,
-              borderWidth: 1,
-              borderColor: COLORS.grey,
-              marginRight: 4,
-              borderRadius: 50,
-            }}
-          >
-            <Image
-              source={require('../assets/facebook.png')}
-              style={{
-                height: 36,
-                width: 36,
-                marginRight: 8,
-              }}
-              resizeMode='contain'
-            />
-
-            <Text>Facebook</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'center',
-            marginVertical: 22,
-          }}
-        >
-          <Text style={{ fontSize: 16, color: COLORS.black }}>
-            Don't have an account ?{' '}
-          </Text>
-          <Pressable onPress={() => navigation.navigate('Signup')}>
-            <Text
-              style={{
-                fontSize: 16,
-                color: COLORS.primary,
-                fontWeight: 'bold',
-                marginLeft: 6,
-              }}
-            >
-              Sign Up
-            </Text>
-          </Pressable>
-        </View>
-      </View>
-    </SafeAreaView>
-  );
-
-
 }
-
